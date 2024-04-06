32ac133983f2274fd21f67db2f540baaee83a625
Shipochki <121862582+Shipochki@users.noreply.github.com>
2024-04-06 21:05:29 +0300

    Updated references

--- TravelBuddies.Infrastructure/Database/TravelBuddiesDbContext.cs
+++ TravelBuddies.Infrastructure/Database/TravelBuddiesDbContext.cs
@@ -1,8 +1,29 @@
 ﻿namespace TravelBuddies.Infrastructure.Database
 {
 	using Microsoft.EntityFrameworkCore;
+	using TravelBuddies.Domain.Entities;
 
 	public class TravelBuddiesDbContext : DbContext
 	{
+		public TravelBuddiesDbContext(DbContextOptions<TravelBuddiesDbContext> options)
+			: base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			List<Role> roles = new List<Role>()
+			{
+				{ new Role() { Id = 1, Name = "Client" } },
+				{ new Role() { Id = 2, Name = "Driver"} },
+				{ new Role() { Id = 3, Name = "Admin"} }
+			};
+
+			modelBuilder.Entity<Role>()
+				.HasData(roles);
+
+			base.OnModelCreating(modelBuilder);
+		}
 	}
 }
