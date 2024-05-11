8ed9a1a5e75adde8e8d11e9c481761f9ba129692
Shipochki <121862582+Shipochki@users.noreply.github.com>
2024-05-11 20:39:19 +0300

    Created tests for get posts by search and for creating review

---
+++ TravelBuddies.UnitTests/Post/Queries/GetPostsBySearchHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿namespace TravelBuddies.UnitTests.Post.Queries
+{
+	using TravelBuddies.Application.Post.Queries.GetPostsBySearch;
+	using TravelBuddies.Domain.Entities;
+
+	public class GetPostsBySearchHandlerTests : BaseHandlerTests
+	{
+		[Fact]
+		public async Task GetPostsBySearch_WithOnlyCities_ShouldReturnPosts()
+		{
+			//Arrange
+			var handler = new GetPostBySearchHandler(_repostiory, _userManager, _roleManager);
+
+			var user = new ApplicationUser { UserName = "testuser", Email = "test@example.com" };
+			var country = new Country() { Name = "country" };
+			var city1 = new City() { Country = country, Name = "Sofia" };
+			var city2 = new City() { Country = country, Name = "Targovishte" };
+			var post = new Post
+			{
+				Creator = user,
+				CreatorId = user.Id,
+				Description = "test",
+				FromDestinationCity = city1,
+				ToDestinationCity = city2
+			};
+
+			await _dbContext.AddAsync(post);
+			await _dbContext.SaveChangesAsync();
+
+			var query = new GetPostBySearchQuery()
+			{
+				FromDestinationCityId = city1.Id,
+				ToDestinationCityId = city2.Id,
+			};
+
+			//Act
+			var result = await handler.Handle(query, default);
+
+			//Assert
+			Assert.Equal(post, result.First());
+		}
+	}
+}
---
+++ TravelBuddies.UnitTests/Review/Commands/CreateReviewHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿namespace TravelBuddies.UnitTests.Review.Commands
+{
+	using TravelBuddies.Application.Exceptions;
+	using TravelBuddies.Application.Review.Commands.CreateReview;
+	using TravelBuddies.Domain.Entities;
+
+	public class CreateReviewHandlerTests : BaseHandlerTests
+	{
+		[Fact]
+		public void CreateReview_WithNonExistingCreator_ShouldThrowsException()
+		{
+			//Arrange
+			var handler = new CreateReviewHandler(_repostiory, _userManager, _roleManager);
+			var command = new CreateReviewCommand(){CreatorId = "1", ReciverId = "2" };
+
+			//Assert
+			Assert.ThrowsAsync<ApplicationUserNotFoundException>(async () =>
+			{
+				await handler.Handle(command, default);
+			});
+		}
+
+		[Fact]
+		public async Task CreateReview_WithNonExistingReciver_ShouldThrowsException()
+		{
+			//Arrange
+			var handler = new CreateReviewHandler(_repostiory, _userManager, _roleManager);
+			
+			var user = new ApplicationUser() { UserName = "Test", Email = "email"};
+
+			await _dbContext.AddAsync(user);
+			await _dbContext.SaveChangesAsync();
+
+			var command = new CreateReviewCommand() { CreatorId = user.Id, ReciverId = "1" };
+
+			//Assert
+			await Assert.ThrowsAsync<ApplicationUserNotFoundException>(async () =>
+			{
+				await handler.Handle(command, default);
+			});
+		}
+
+		[Fact]
+		public async Task CreateReview_WithValidData_ShouldCreateReview()
+		{
+			//Arrange
+			var handler = new CreateReviewHandler(_repostiory, _userManager, _roleManager);
+
+			var user1 = new ApplicationUser() { UserName = "Test1", Email = "email1" };
+			var user2 = new ApplicationUser() { UserName = "Test2", Email = "email2" };
+
+			await _dbContext.AddRangeAsync(user1, user2);
+			await _dbContext.SaveChangesAsync();
+
+			var command = new CreateReviewCommand()
+			{
+				CreatorId = user1.Id,
+				ReciverId = user2.Id,
+				Text = "test",
+				Rating = 4,
+			};
+
+			//Act
+			var result = await handler.Handle(command, default);
+
+			//Assert
+			Assert.Equal(user1.Id, result.CreatorId);
+			Assert.Equal(user2.Id, result.ReciverId);
+			Assert.Equal("test", result.Text);
+			Assert.Equal(4, result.Rating);
+		}
+	}
+}
