31d3ea5a465eca7880597f8021538e34e68418fa
Shipochki <121862582+Shipochki@users.noreply.github.com>
2024-05-13 01:20:45 +0300

    Created tests for delete user group and one test for remove user group only if user for remove is null

---
+++ TravelBuddies.UnitTests/UserGroup/Commands/DeleteUserGroupHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿namespace TravelBuddies.UnitTests.UserGroup.Commands
+{
+	using Microsoft.EntityFrameworkCore;
+	using TravelBuddies.Application.Exceptions;
+	using TravelBuddies.Application.UserGroup.Commands.DeleteUserGroup;
+	using TravelBuddies.Domain.Entities;
+
+	public class DeleteUserGroupHandlerTests : BaseHandlerTests
+	{
+		[Fact]
+		public void DeleteUserGroup_WithUserNotInGroup_ShouldThrowsException()
+		{
+			//Arrange
+			var handler = new DeleteUserGroupHandler(_repostiory, _userManager, _roleManager);
+			var command = new DeleteUserGroupCommand() { UserId = "1" };
+
+			//Assert
+			Assert.ThrowsAsync<ApplicationUserNotInGroupException>(async () =>
+			{
+				await handler.Handle(command, default);
+			});
+		}
+
+		[Fact]
+		public async Task DeleteUserGroup_WithValidData_ShouldDeleteUserGroup()
+		{
+			//Arrange
+			var handler = new DeleteUserGroupHandler(_repostiory, _userManager, _roleManager);
+
+			var user = new ApplicationUser { UserName = "testuser", Email = "test@example.com" };
+			var country = new Country() { Name = "country" };
+			var city1 = new City() { Country = country, Name = "Sofia" };
+			var city2 = new City() { Country = country, Name = "Targovishte" };
+			var post = new Post
+			{
+				Creator = user,
+				CreatorId = user.Id,
+				Description = "test",
+				FromDestinationCity = city1,
+				ToDestinationCity = city2
+			};
+			var group = new Group()
+			{
+				Creator = user,
+				CreatorId = user.Id,
+				Post = post,
+				PostId = post.Id,
+				CreatedOn = DateTime.UtcNow,
+			};
+			var userGroup = new UserGroup()
+			{
+				Group = group,
+				User = user,
+				UserId = user.Id,
+			};
+
+			await _dbContext.AddAsync(userGroup);
+			await _dbContext.SaveChangesAsync();
+
+			var command = new DeleteUserGroupCommand()
+			{
+				UserId = user.Id,
+				GroupId = group.Id,
+			};
+
+			//Act
+			await handler.Handle(command, default);
+			var result = await _dbContext.UsersGroups.FirstOrDefaultAsync();
+
+			//Assert
+			Assert.Null(result);
+		}
+	}
+}
---
+++ TravelBuddies.UnitTests/UserGroup/Commands/RemoveUserGroupHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace TravelBuddies.UnitTests.UserGroup.Commands
+{
+	using TravelBuddies.Application.Exceptions;
+	using TravelBuddies.Application.UserGroup.Commands.RemoveUserGroup;
+
+	public class RemoveUserGroupHandlerTests : BaseHandlerTests
+	{
+		[Fact]
+		public void RemoveUserGroup_WithNonExistingUserForRemove_ShouldThrowsException()
+		{
+			//Arrange
+			var handler = new RemoveUserGroupHandler(_repostiory, _userManager, _roleManager);
+			var command = new RemoveUserGroupCommand()
+			{
+				OwnerId = "1",
+				UserIdForRemove = "1",
+			};
+
+			//Assert
+			Assert.ThrowsAsync<ApplicationUserNotFoundException>(async () =>
+			{
+				await handler.Handle(command, default);
+			});
+		}
+	}
+}
